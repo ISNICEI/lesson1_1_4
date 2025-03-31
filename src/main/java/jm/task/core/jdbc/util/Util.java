48669b603f8f4c2e11c2241cd50cb1087f066900
ISNICEI <49389754+ISNICEI@users.noreply.github.com>
2025-03-31 12:15:06 +0300

    Проверка связи

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+  private static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest";
+  private static final String DB_USER = "root";
+  private static final String DB_PASSWORD = "162160Zz";
+  private static Connection connection;
+
+  public static Connection getConnection() {
+    try {
+      return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
